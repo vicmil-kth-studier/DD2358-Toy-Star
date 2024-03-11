01be5367fac34685600255e998d7da79800bf063
vnomarrow <vnomarrow@gmail.com>
2024-03-11 22:03:47 +0100

    update __init__.py so you don't have to have cuda installed to run simulation on cpu

--- simulation/__init__.py
+++ simulation/__init__.py
@@ -7,9 +7,6 @@ from timeit import default_timer as timer
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
-import gpu
-import cpu
-import original
 
 def plot_different_n(max_n : int, step : int, times : int = 1, show_cpu: bool = False, show_gpu: bool = False, show_original: bool = False, log : bool = False, save : bool = False) -> None:
     """ Plot performance of different optimizations
@@ -35,14 +32,17 @@ def plot_different_n(max_n : int, step : int, times : int = 1, show_cpu: bool = 
     names = []
     
     if show_cpu:
+        import cpu
         functions.append(cpu.sim)
         names.append("CPU (NumPy)")
         
     if show_gpu:
+        import gpu
         functions.append(gpu.sim)
         names.append("GPU (CuPy)")
     
     if show_original:
+        import original
         functions.append(original.sim)
         names.append("Original (NumPy)")
 
@@ -76,9 +76,75 @@ def plot_different_n(max_n : int, step : int, times : int = 1, show_cpu: bool = 
     plt.xlabel('particle count')
     plt.ylabel('time [s]')
     if save:
-        plt.savefig(f"images\\{"c" if show_cpu else ""}{"g" if show_gpu else ""}{"o" if show_original else ""}{"_log" if log else ""}_n{max_n}_s{step}_t{times}.png")
+        plt.savefig(f"""images\\{"c" if show_cpu else ""}{"g" if show_gpu else ""}{"o" if show_original else ""}{"_log" if log else ""}_n{max_n}_s{step}_t{times}.png""")
     else:
         plt.show()
+
+
+'''def main():
+	""" SPH simulation """
+	
+	# Simulation parameters
+	N         = 400    # Number of particles
+	t         = 0      # current time of the simulation
+	tEnd      = 12     # time at which simulation ends
+	dt        = 0.04   # timestep
+	M         = 2      # star mass
+	R         = 0.75   # star radius
+	h         = 0.1    # smoothing length
+	k         = 0.1    # equation of state constant
+	n         = 1      # polytropic index
+	nu        = 1      # damping
+	plotRealTime = True # switch on for plotting as the simulation goes along
+	
+	# Generate Initial Conditions
+	np.random.seed(42)            # set the random number generator seed
+	
+	lmbda = 2*k*(1+n)*np.pi**(-3/(2*n)) * (M*gamma(5/2+n)/R**3/gamma(1+n))**(1/n) / R**2  # ~ 2.01
+	m     = M/N                    # single particle mass
+	pos   = np.random.randn(N,3)   # randomly selected positions and velocities
+	vel   = np.zeros(pos.shape)
+	
+	# calculate initial gravitational accelerations
+	acc = getAcc( pos, vel, m, h, k, n, lmbda, nu )
+	
+	# number of timesteps
+	Nt = int(np.ceil(tEnd/dt))
+	
+	# prep figure
+	fig = plt.figure(figsize=(4,5), dpi=80)
+	grid = plt.GridSpec(3, 1, wspace=0.0, hspace=0.3)
+	ax1 = plt.subplot(grid[0:2,0])
+	ax2 = plt.subplot(grid[2,0])
+	rr = np.zeros((100,3))
+	rlin = np.linspace(0,1,100)
+	rr[:,0] =rlin
+	rho_analytic = lmbda/(4*k) * (R**2 - rlin**2)
+	
+	# Simulation Main Loop
+	for i in range(Nt):
+		# (1/2) kick
+		vel += acc * dt/2
+		
+		# drift
+		pos += vel * dt
+		
+		# update accelerations
+		acc = getAcc( pos, vel, m, h, k, n, lmbda, nu )
+		
+		# (1/2) kick
+		vel += acc * dt/2
+		
+		# update time
+		t += dt
+		
+		# get density for plotting
+		rho = getDensity( pos, pos, m, h )
+		
+		# plot in real time
+		if plotRealTime or (i == Nt-1):
+			plt.sca(ax1)'''
+               
 
 if __name__ == "__main__":
     args_dict = dict()
@@ -109,18 +175,21 @@ if __name__ == "__main__":
             log=args_dict["log"] == "True",
             save=args_dict["save"] == "True")
     elif args_dict["plot"] == "gpu":
+        import gpu
         gpu.sim(
             plot=True,
             plot_2d=True,
             plot_real_time=True,
             n=int(args_dict["n"]))
     elif args_dict["plot"] == "cpu":
+        import cpu
         cpu.sim(
             plot=True,
             plot_2d=True,
             plot_real_time=True,
             n=int(args_dict["n"]))
     elif args_dict["plot"] == "original":
+        import original
         original.sim(
             plot=True,
             plot_2d=True,
